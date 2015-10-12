4aa8b0727c946814fcb668a8b4e6d0553b9e3953
Ludovic Horem <ludo@MacBook-Air-de-Ludovic.local>
2015-10-12 13:37:56 +0200

    add bootstrap and .lock files

---
+++ app/resources/views/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>A.D.N</title>
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet" type="text/css">
+    <style>
+        html, body {
+            height: 100%;
+        }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Indie Flower';
+            text-align: center;
+        }
+        h1 {
+            font-size: 96px;
+        }
+        #wrapper {
+            margin-top: 200px;    
+        }
+    </style>
+</head>
+<body>
+    <div id="wrapper">
+        <h3>Welcome to</h3>
+        <h1>A.D.N</h1>
+    </div>
+</body>
+</html>
--- app/routes.php
+++ app/routes.php
@@ -1,18 +1,11 @@
 <?php
 
 use Skull\Routing\Route;
+use Skull\View\View;
 
-Route::get('/', 'IndexController#home');
-
-Route::get("/lol/{name}/post/{age}", function($name, $age) {
-    return $name . " est un idiot qui a " . $age . " ans !";
+Route::get('/', function() {
+    return View::make('home');
 });
-
-
-Route::post('/user', 'UserController#store');
-Route::get('/users', 'UserController#index');
-Route::get('/user', 'UserController#create');
-Route::get('/users/{id}', 'UserController#show');
 
 Route::otherwise(function() {
     header('HTTP/1.0 404 Not Found');
